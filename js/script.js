db011dc5bf0fd9445cb4ff8942fcf938ed50f332
Michał Micherda <mmicherdakr@gmail.com>
2022-11-23 21:21:04 +0100

    Delete js folder

--- js/script.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-  var x = document.querySelector(".header_mobile");
-  if (x.style.display === "block") {
-    x.style.color = "green";
-  } else {
-    x.style.display = "block";
-  }
-}
